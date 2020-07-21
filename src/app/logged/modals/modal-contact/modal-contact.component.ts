29dcbd1cd5c4ff9487bacd287b0036d75dc9de04
yesid davila <elmondoles@gmail.com>
2020-07-21 15:29:18 -0500

    confirmacion de prueba

--- src/app/logged/modals/modal-contact/modal-contact.component.ts
+++ src/app/logged/modals/modal-contact/modal-contact.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalContactComponent implements OnInit {
 
-  public title = 'Dejanos un Like';
+  public title = 'Dejanos un Like...';
   constructor() { }
 
   ngOnInit(): void {
