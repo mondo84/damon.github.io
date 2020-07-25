eb664fcaf1599736bc323d8cf829f8683cc3b924
yesid davila <elmondoles@gmail.com>
2020-07-24 19:27:03 -0500

    + menu circular en home

--- src/app/logged/home/inicio/inicio.component.ts
+++ src/app/logged/home/inicio/inicio.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.scss']
 })
-export class InicioComponent implements OnInit {
+export class InicioComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  toggleClass = true;
+  @ViewChild('btnRef', {  static: true }) btnRef: MatButton;    // Elemento externo de material design.
+  @ViewChild('menuRef', {  static: true }) menuRef: ElementRef;  // Elemento nativo del componente.
+
+  constructor(private objR2: Renderer2) {
+  }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    // Se agrega listener al boton de material design.
+    this.objR2.listen(this.btnRef._elementRef.nativeElement, 'click', (e) => {
+      // console.log(e);
+
+      if (this.toggleClass) {
+        this.objR2.addClass(this.btnRef._elementRef.nativeElement, 'pulsed');
+        this.objR2.addClass(this.menuRef.nativeElement, 'expand');
+      } else {
+        this.objR2.removeClass(this.btnRef._elementRef.nativeElement, 'pulsed');
+        this.objR2.removeClass(this.menuRef.nativeElement, 'expand');
+      }
+
+      this.toggleClass = !this.toggleClass;
+    });
   }
 
 }
